b0e83fa0806cbf2a992e6008a97b806edbe173a9
usman.khan@whizzbridge.com <usman.khan@whizzbridge.com>
2024-05-20 13:47:02 +0500

    Metadata Origami

---
+++ Pages/MetadataOrigami.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using SAMLTEST.SAMLObjects;
+using System;
+
+namespace SAMLTEST.Pages
+{
+    /// <summary>
+    /// This is the Metadata Page Model
+    /// </summary>
+    
+}
